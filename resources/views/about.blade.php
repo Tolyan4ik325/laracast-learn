1d0482cf3f502d2011cd8a24310401a69b41ab1a
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-08-23 00:15:31 +0300

    work with blades

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+
+@section('title', 'About')
+
+
+@section('content')
+
+		<h1>About us</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore adipisci odit voluptates ipsum vero nobis. Sint praesentium facilis quisquam animi numquam vero, nihil, blanditiis soluta, itaque laborum quo totam magnam architecto iusto. Debitis perspiciatis omnis, aut, iure minima optio blanditiis. Sunt, numquam aliquid eius iure nesciunt quos explicabo consequatur consectetur odio dignissimos iste natus sed excepturi? Excepturi blanditiis maiores quidem consequuntur atque necessitatibus maxime voluptas doloremque sint deleniti, voluptate quaerat fugiat aperiam non illum placeat aliquid numquam, beatae deserunt itaque pariatur nemo! Illo dicta ipsum possimus praesentium consectetur dolore rem officiis repudiandae doloribus magni, blanditiis sequi voluptatibus, quis optio tempore.</p>
+
+@endsection 
+
+
+
+
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+
+@section('content')
+
+			<h1>Contact</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, reiciendis.</p>
+
+@endsection 
